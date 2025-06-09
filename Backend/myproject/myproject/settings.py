c57810bdf4a6e185679b56a4bce39cc4155162c7
Abhishek Kumar <abhikumar21@iitk.ac.in>
2025-06-10 01:31:11 +0530

    all hosts allowed

--- Backend/myproject/myproject/settings.py
+++ Backend/myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s6yx4^qjl2dm$#n3ug3adid&1^&swp_ig*kk7suk+#7%kwtm4=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
